dd2eb66f5f75c1e37af5174eda0f6860eac678cc
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2023-03-06 19:03:36 +0530

    Add files via upload

---
+++ NodeJS/index.js
@@ -0,0 +1,22 @@
+// //jshint esversion:6
+
+// const fs = require("fs");
+
+// fscopyFileSync("text1.txt","file2.txt")
+
+
+// console.log("hello world")
+
+// var superhero=require("superheroes")
+
+// var my=superhero.random()
+
+const superheroes = require('superheroes');
+var my=superheroes.random();
+
+
+const supervillains = require('supervillains');
+var myv=supervillains.random();
+
+
+console.log(myv)
